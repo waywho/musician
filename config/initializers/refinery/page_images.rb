44e706c674562b3030c1f91b1dbfddcfb3b50490
WeiHsi Hu <weihsi.hu@gmail.com>
2016-05-19 22:33:01 +0000

    add production show page and gallery

--- config/initializers/refinery/page_images.rb
+++ config/initializers/refinery/page_images.rb
@@ -1,5 +1,5 @@
 Refinery::PageImages.configure do |config|
   # config.captions = false
-  config.enable_for = [{:model=>"Refinery::Page", :tab=>"Refinery::Pages::Tab"}, {:model=>"Refinery::Production", :tab=>"Refinery::Productions::Tab"}]
+  config.enable_for = [{:model=>"Refinery::Page", :tab=>"Refinery::Pages::Tab"}, {:model=>"Refinery::Productions::Production", :tab=>"Refinery::Productions::Tab"}]
   # config.wysiwyg = true
 end
